d62e70b3a98da9d72ea0163c55f94753d77ab443
whitemarcus406 <90864595+whitemarcus406@users.noreply.github.com>
2021-11-23 15:51:54 -0500

    Add files via upload

---
+++ EX_9.3.py
@@ -0,0 +1,19 @@
+dictionary_addresses = dict()
+fname = input('Enter file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 2 or words[0] != 'From':
+        continue
+    else:
+        if words[1] not in dictionary_addresses:
+            dictionary_addresses[words[1]] = 1
+        else:
+            dictionary_addresses[words[1]] += 1     
+
+print(dictionary_addresses)
